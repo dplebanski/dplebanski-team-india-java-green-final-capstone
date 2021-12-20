80de7c53cb30f79d67368175c0ed76f1b7c969e8
dplebanski <90109459+dplebanski@users.noreply.github.com>
2021-12-20 13:52:19 -0500

    Delete NewExample.vue

--- vue/src/components/NewExample.vue
+++
@@ -1,5 +0,0 @@
-   <form action="submit">
-      <input type="text" v-model="newExample.title" />
-      <textarea type="text" v-model="newExample.code_example"> </textarea>
-      <button v-on:click.prevent="submitCode">press me!</button>
-    </form>
